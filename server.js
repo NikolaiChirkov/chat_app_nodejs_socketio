872ea79f897c682762c3a723ffa2ba5885bc4321
Nikolai Chirkov <qwertymonstro@gmail.com>
2020-10-21 21:25:26 +0300

    add notification about user connection

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const io = require('socket.io')(3000);
 
+const users = {};
+
 io.on('connection', socket => {
+    socket.on('new-user', name => {
+        users[socket.id] = name;
+        socket.broadcast.emit('user-connected', name);
+    });
     socket.emit('chat-message', 'Hello world');
     socket.on('send-chat-message', message => {
-        console.log(message);
+        socket.broadcast.emit('chat-message', message);
     });
 })
